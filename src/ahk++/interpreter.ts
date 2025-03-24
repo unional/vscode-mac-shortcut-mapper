a8a702a6f068f918c7a503e0f48a6033d49f33f9
Homa Wong <homawong@gmail.com>
2025-03-24 00:47:59 -0700

    fix: use hardcoded path for now

--- src/ahk++/interpreter.ts
+++ src/ahk++/interpreter.ts
@@ -2,14 +2,15 @@ import { workspace } from 'vscode'
 import { configPrefix, interpreterPathV2 } from './constants'
 
 export function getInterpreterPath() {
-	const interpreterPathV1 = 'v1.file.interpreterPath'
-	const AHKConfig = workspace.getConfiguration(configPrefix)
-	if (AHKConfig.has(interpreterPathV2)) {
-		return AHKConfig.get<string>(interpreterPathV2)
-	}
-	if (AHKConfig.has(interpreterPathV1)) {
-		return AHKConfig.get<string>(interpreterPathV1)
-	}
+	return 'C:\\Program Files\\AutoHotkey\\v2\\AutoHotkey.exe'
+	// const interpreterPathV1 = 'v1.file.interpreterPath'
+	// const AHKConfig = workspace.getConfiguration(configPrefix)
+	// if (AHKConfig.has(interpreterPathV2)) {
+	// 	return AHKConfig.get<string>(interpreterPathV2)
+	// }
+	// if (AHKConfig.has(interpreterPathV1)) {
+	// 	return AHKConfig.get<string>(interpreterPathV1)
+	// }
 
-	return undefined
+	// return undefined
 }
